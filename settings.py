6252c70cc4703330d84ae4aaf062236dfbc12890
extraname <zorraider@gmail.com>
2020-02-14 11:23:55 +0200

    add settings1 for secure key

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import logging
 import psycopg2
-
+import settings1
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgress://username:password/db"
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = settings1.settings
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 api = Api(app)
